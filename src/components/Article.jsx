edd6eff75f40f03553e98a2823060f825c114864
AnyaVeselova <anna.correspondence@outlook.com>
2024-06-18 18:22:10 +0100

    conditionally displays loading elements on home page and on article page

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -7,7 +7,6 @@ import {
   CardActions,
   Box,
   CircularProgress,
-  Skeleton,
 } from "@mui/material";
 import { fetchArticleById } from "../utils/api";
 import { useParams } from "react-router-dom";
@@ -16,17 +15,6 @@ import { useEffect, useState } from "react";
 export default function Article({ article }) {
   const { article_id } = useParams();
   const [articleWithBody, setArticleWithoBody] = useState(null);
-
-  const centeredCard = article_id
-    ? {
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        width: "100%",
-        margin: "6em 0",
-      }
-    : {};
 
   useEffect(() => {
     fetchArticleById(!article_id ? article.article_id : article_id).then(
@@ -40,129 +28,138 @@ export default function Article({ article }) {
     return created_at.split("T")[0];
   }
 
-  if (!articleWithBody) {
-    return <Skeleton variant="rect" width={210} height={118} />;
+  //conditional rendering of loading elements on Home page and on Article page
+  if (!articleWithBody && article_id) {
+    return (
+      <Card
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          width: "100vw",
+        }}
+      >
+        <CircularProgress width={210} height={118} />
+      </Card>
+    );
+  } else if (!articleWithBody && !article_id) {
+    return (
+      <CircularProgress
+        sx={{ justifySelf: "center" }}
+        width={210}
+        height={118}
+      />
+    );
   }
 
   return (
-    <Box sx={centeredCard}>
-      <Card
+    <Card
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100%",
+        width: !article_id ? "90%" : "60%",
+        margin: !article_id ? "0 auto" : "7em auto",
+      }}
+    >
+      <CardMedia
+        component="img"
+        image={articleWithBody.article_img_url}
+        alt={articleWithBody.title}
+      />
+      <Box
         sx={{
           display: "flex",
           flexDirection: "column",
           height: "100%",
-          width: "60%",
-          margin: "0 auto",
+          justifyContent: "space-between",
         }}
       >
-        <CardMedia
-          component="img"
-          image={articleWithBody.article_img_url}
-          alt={articleWithBody.title}
-          height="50%"
-          sx={{ objectFit: "cover" }}
-        />
         <Box
           sx={{
             display: "flex",
-            flexDirection: "column",
-            height: "100%",
+            alignItems: "center",
             justifyContent: "space-between",
+            backgroundColor: "#ADD8E6",
+            padding: "8px 16px",
+            gap: "16px",
           }}
         >
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "space-between",
-              backgroundColor: "#ADD8E6",
-              padding: "8px 16px",
-              gap: "16px",
-            }}
-          >
-            <Typography gutterBottom variant="body2" color="text.secondary">
-              <Typography
-                variant=""
-                component="span"
-                sx={{ fontWeight: "bold" }}
-              >
-                topic:
-              </Typography>{" "}
-              {articleWithBody.topic}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              <Typography
-                variant="body2"
-                component="span"
-                sx={{ fontWeight: "bold" }}
-              >
-                author:{" "}
-              </Typography>
-              {articleWithBody.author}
-            </Typography>
+          <Typography gutterBottom variant="body2" color="text.secondary">
+            <Typography variant="" component="span" sx={{ fontWeight: "bold" }}>
+              topic:
+            </Typography>{" "}
+            {articleWithBody.topic}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
             <Typography
-              flexBasis="150px"
               variant="body2"
-              color="text.secondary"
+              component="span"
+              sx={{ fontWeight: "bold" }}
             >
-              <Typography
-                variant="body2"
-                component="span"
-                sx={{ fontWeight: "bold" }}
-              >
-                date:
-              </Typography>{" "}
-              {formatDate(articleWithBody.created_at)}
+              author:{" "}
             </Typography>
-          </Box>
-          <Box sx={{ padding: "16px" }}>
-            <Typography mb="10px" lineHeight={1.2} variant="h6">
-              {articleWithBody.title}
-            </Typography>
+            {articleWithBody.author}
+          </Typography>
+          <Typography flexBasis="150px" variant="body2" color="text.secondary">
             <Typography
-              variant="body1"
-              sx={{
-                display: "-webkit-box",
-                WebkitBoxOrient: "vertical",
-                overflow: "hidden",
-                WebkitLineClamp: !article_id ? 3 : null,
-              }}
+              variant="body2"
+              component="span"
+              sx={{ fontWeight: "bold" }}
             >
-              {articleWithBody.body}
-            </Typography>
-            {!article_id && <Button>Read more</Button>}
-          </Box>
-          <Box
+              date:
+            </Typography>{" "}
+            {formatDate(articleWithBody.created_at)}
+          </Typography>
+        </Box>
+        <Box sx={{ padding: "16px" }}>
+          <Typography mb="10px" lineHeight={1.2} variant="h6">
+            {articleWithBody.title}
+          </Typography>
+          <Typography
+            variant="body1"
             sx={{
-              display: "flex",
-              justifyContent: "space-between",
-              backgroundColor: "#ADD8E6",
-              padding: "8px 16px",
+              display: "-webkit-box",
+              WebkitBoxOrient: "vertical",
+              overflow: "hidden",
+              WebkitLineClamp: !article_id ? 3 : null,
             }}
           >
-            <Typography variant="body2" color="text.secondary">
-              <Typography
-                variant="body2"
-                component="span"
-                sx={{ fontWeight: "bold" }}
-              >
-                Votes:
-              </Typography>{" "}
-              {articleWithBody.votes}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              <Typography
-                variant="body2"
-                component="span"
-                sx={{ fontWeight: "bold" }}
-              >
-                Comments:
-              </Typography>{" "}
-              {articleWithBody.comment_count}
-            </Typography>
-          </Box>
+            {articleWithBody.body}
+          </Typography>
+          {!article_id && <Button>Read more</Button>}
         </Box>
-      </Card>
-    </Box>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            backgroundColor: "#ADD8E6",
+            padding: "8px 16px",
+          }}
+        >
+          <Typography variant="body2" color="text.secondary">
+            <Typography
+              variant="body2"
+              component="span"
+              sx={{ fontWeight: "bold" }}
+            >
+              Votes:
+            </Typography>{" "}
+            {articleWithBody.votes}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            <Typography
+              variant="body2"
+              component="span"
+              sx={{ fontWeight: "bold" }}
+            >
+              Comments:
+            </Typography>{" "}
+            {articleWithBody.comment_count}
+          </Typography>
+        </Box>
+      </Box>
+    </Card>
   );
 }
